8dc0350f835770910e8d898c5c46c567cca30a34
bulentselcuk <bulentselcuk@gmail.com>
2020-05-01 14:27:59 -0500

    Fibonacci

---
+++ src/Java_Tec/LargestNumberInArray.java
@@ -0,0 +1,12 @@
+package Java_Tec;
+
+import java.util.Arrays;
+
+public class LargestNumberInArray {
+    public static void main(String[] args) {
+
+        int [] arr = {5, 6, 76, 31, 43, 1};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-1]);
+    }
+}
